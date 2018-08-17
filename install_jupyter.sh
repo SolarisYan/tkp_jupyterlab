c1fe7e02dfc38977afa331fd5c46c1f28f734522
Tim Paine <t.paine154@gmail.com>
2018-08-17 10:39:02 -0400

    .

--- install_jupyter.sh
+++ install_jupyter.sh
@@ -6,6 +6,7 @@ jupyter labextension install @jupyterlab/statusbar > /dev/null 2>&1 || echo "Ext
 jupyter labextension install nbdime-jupyterlab > /dev/null 2>&1 || echo "Extension failed to install: nbdime-jupyterlab"
 jupyter labextension install jupyterlab_iframe > /dev/null 2>&1 || echo "Extension failed to install: jupyterlab_iframe"
 jupyter labextension install jupyterlab_html > /dev/null 2>&1 || echo "Extension failed to install: jupyterlab_html"
+jupyter labextension install jupyterlab_email > /dev/null 2>&1 || echo "Extension failed to install: jupyterlab_email"
 jupyter labextension install jupyterlab_commands > /dev/null 2>&1 || echo "Extension failed to install: jupyterlab_commands"
 jupyter labextension install jupyterlab_templates > /dev/null 2>&1 || echo "Extension failed to install: jupyterlab_templates"
 jupyter labextension install jupyterlab-drawio > /dev/null 2>&1 || echo "Extension failed to install: jupyterlab-drawio"
@@ -20,6 +21,7 @@ jupyter labextension install qgrid > /dev/null 2>&1 || echo "Extension failed to
 jupyter labextension install ipyvolume > /dev/null 2>&1 || echo "Extension failed to install: ipyvolume"
 jupyter labextension install jupyter-threejs > /dev/null 2>&1 || echo "Extension failed to install: jupyter-threejs"
 jupyter labextension install jupyter-leaflet > /dev/null 2>&1 || echo "Extension failed to install: jupyter-leaflet"
+jupyter labextension install @jupyter-widgets/jupyterlab-sidecar > /dev/null 2>&1 || echo "Extension failed to install: @jupyter-widgets/jupyterlab-sidecar"
 jupyter labextension install @oriolmirosa/jupyterlab_materialdarker > /dev/null 2>&1 || echo "Extension failed to install: @oriolmirosa/jupyterlab_materialdarker"
 jupyter labextension install @jpmorganchase/perspective-jupyterlab > /dev/null 2>&1 || echo "Extension failed to install: @jpmorganchase/perspective-jupyterlab"
 
@@ -28,4 +30,5 @@ jupyter serverextension enable --py nbdime
 jupyter serverextension enable --py jupyterlab_iframe
 jupyter serverextension enable --py jupyterlab_templates
 jupyter serverextension enable --py jupyterlab_commands
+jupyter serverextension enable --py jupyterlab_email
 jupyter serverextension enable --py nbresuse
